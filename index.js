19063a8d278473e26929778098bc86ff138a6603
Richard Clarke <richard-clarke@outlook.com>
2014-10-13 12:19:08 +0100

    Updated the copyright etc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Copyright © 2009-2014 E-Tale Marketing Solutions Ltd.
+// All rights are reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
 
 /**
  * Export lib/mongoose
